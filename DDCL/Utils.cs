fdb008ff545226035cd94e8dcf8ba69650ed811d
Noah Stolk <noah.stolk1997@gmail.com>
2019-05-17 12:24:16 +0200

    no message

--- DDCL/Utils.cs
+++ DDCL/Utils.cs
@@ -7,8 +7,6 @@ namespace DDCL
 {
 	public static class Utils
 	{
-		public const string V3Hash = "569fead87abf4d30fdee4231a6398051";
-
 		public static string SurvivalFilePath = Path.Combine(Path.GetDirectoryName(Scanner.Instance.Process.MainModule.FileName), "dd", "survival");
 
 		public static string CalculateSpawnsetHash()
