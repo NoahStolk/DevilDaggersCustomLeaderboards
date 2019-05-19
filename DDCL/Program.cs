345947ebd3ca323c62fd8ad60f8a957ff0ad0745
Noah Stolk <noah.stolk1997@gmail.com>
2019-05-19 13:29:45 +0200

    Make console unresizable.

--- DDCL/Program.cs
+++ DDCL/Program.cs
@@ -3,6 +3,7 @@ using DDCL.Network;
 using log4net;
 using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace DDCL
@@ -14,6 +15,20 @@ namespace DDCL
 	/// </summary>
 	public static class Program
 	{
+		const int MF_BYCOMMAND = 0x00000000;
+		const int SC_MINIMIZE = 0xF020;
+		const int SC_MAXIMIZE = 0xF030;
+		const int SC_SIZE = 0xF000;
+
+		[DllImport("user32.dll")]
+		public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+		[DllImport("user32.dll")]
+		private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+		[DllImport("kernel32.dll", ExactSpelling = true)]
+		private static extern IntPtr GetConsoleWindow();
+
 		private static readonly Scanner scanner = Scanner.Instance;
 		private static bool recording = true;
 
@@ -23,6 +38,11 @@ namespace DDCL
 		{
 			Console.CursorVisible = false;
 			Console.WindowHeight = 40;
+
+			DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MINIMIZE, MF_BYCOMMAND);
+			DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MAXIMIZE, MF_BYCOMMAND);
+			DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_SIZE, MF_BYCOMMAND);
+
 			Console.Title = $"Devil Daggers Custom Leaderboards - {Utils.GetVersion()}";
 
 			Thread.CurrentThread.CurrentCulture = Constants.Culture;
