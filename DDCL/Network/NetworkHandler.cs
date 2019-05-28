e6ab1df24fbf38cdb7c44b758739c0869fec4c63
Noah Stolk <noah.stolk1997@gmail.com>
2019-05-28 11:59:24 +0200

    Move encryption to NetBase.

--- DDCL/Network/NetworkHandler.cs
+++ DDCL/Network/NetworkHandler.cs
@@ -1,6 +1,6 @@
-﻿using AESBaseStandard;
-using DDCL.MemoryHandling;
+﻿using DDCL.MemoryHandling;
 using Newtonsoft.Json;
+using NetBase.Encryption;
 using System;
 using System.Collections.Generic;
 using System.IO;
