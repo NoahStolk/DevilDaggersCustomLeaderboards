81e08f9627fd8ddab925038e14cfec984d6a3096
Noah Stolk <noah.stolk1997@gmail.com>
2020-03-11 17:14:59 +0100

    Update copyright year in AssemblyInfo

--- DevilDaggersCustomLeaderboards/Properties/AssemblyInfo.cs
+++ DevilDaggersCustomLeaderboards/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Devil Daggers Custom Leaderboards")]
-[assembly: AssemblyCopyright("Copyright © Noah Stolk 2019")]
+[assembly: AssemblyCopyright("Copyright © Noah Stolk 2019 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
