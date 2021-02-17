344f866604eaf30e07082258aa1073d4647fb7e1
Noah Stolk <noah.stolk@gmail.com>
2021-02-17 21:07:56 +0100

    Improve code

--- DevilDaggersCustomLeaderboards/Utils/ColorUtils.cs
+++ DevilDaggersCustomLeaderboards/Utils/ColorUtils.cs
@@ -63,24 +63,25 @@ namespace DevilDaggersCustomLeaderboards.Utils
 			if (!brc)
 				return Marshal.GetLastWin32Error();
 
+			ColorReference colorReference = new(r, g, b);
 			switch (colorIndex)
 			{
-				case 0: csbe._black = new ColorReference(r, g, b); break;
-				case 1: csbe._darkBlue = new ColorReference(r, g, b); break;
-				case 2: csbe._darkGreen = new ColorReference(r, g, b); break;
-				case 3: csbe._darkCyan = new ColorReference(r, g, b); break;
-				case 4: csbe._darkRed = new ColorReference(r, g, b); break;
-				case 5: csbe._darkMagenta = new ColorReference(r, g, b); break;
-				case 6: csbe._darkYellow = new ColorReference(r, g, b); break;
-				case 7: csbe._gray = new ColorReference(r, g, b); break;
-				case 8: csbe._darkGray = new ColorReference(r, g, b); break;
-				case 9: csbe._blue = new ColorReference(r, g, b); break;
-				case 10: csbe._green = new ColorReference(r, g, b); break;
-				case 11: csbe._cyan = new ColorReference(r, g, b); break;
-				case 12: csbe._red = new ColorReference(r, g, b); break;
-				case 13: csbe._magenta = new ColorReference(r, g, b); break;
-				case 14: csbe._yellow = new ColorReference(r, g, b); break;
-				case 15: csbe._white = new ColorReference(r, g, b); break;
+				case 0: csbe._black = colorReference; break;
+				case 1: csbe._darkBlue = colorReference; break;
+				case 2: csbe._darkGreen = colorReference; break;
+				case 3: csbe._darkCyan = colorReference; break;
+				case 4: csbe._darkRed = colorReference; break;
+				case 5: csbe._darkMagenta = colorReference; break;
+				case 6: csbe._darkYellow = colorReference; break;
+				case 7: csbe._gray = colorReference; break;
+				case 8: csbe._darkGray = colorReference; break;
+				case 9: csbe._blue = colorReference; break;
+				case 10: csbe._green = colorReference; break;
+				case 11: csbe._cyan = colorReference; break;
+				case 12: csbe._red = colorReference; break;
+				case 13: csbe._magenta = colorReference; break;
+				case 14: csbe._yellow = colorReference; break;
+				case 15: csbe._white = colorReference; break;
 			}
 
 			++csbe._srWindow._bottom;
