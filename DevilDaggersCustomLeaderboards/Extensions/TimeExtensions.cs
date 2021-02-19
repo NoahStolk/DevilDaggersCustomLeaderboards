90ed310b782161f0aadbab7de8986603773fc119
Noah Stolk <noah.stolk@gmail.com>
2021-02-19 15:21:44 +0100

    Fix rounding error

--- DevilDaggersCustomLeaderboards/Extensions/TimeExtensions.cs
+++ DevilDaggersCustomLeaderboards/Extensions/TimeExtensions.cs
@@ -5,9 +5,9 @@ namespace DevilDaggersCustomLeaderboards.Utils
 	public static class TimeExtensions
 	{
 		public static int ConvertToTimeInt(this float timeFloat)
-			=> (int)timeFloat * 10000;
+			=> (int)(timeFloat * 10000);
 
 		public static int ConvertToTimeInt(this FloatVariable timeFloat)
-			=> (int)timeFloat * 10000;
+			=> (int)(timeFloat * 10000);
 	}
 }
