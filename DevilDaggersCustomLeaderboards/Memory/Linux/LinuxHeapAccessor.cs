2e3468caa4ef33c854e7c46ae8e788583e40f525
Noah Stolk <noah.stolk@gmail.com>
2021-09-14 11:57:23 +0200

    Implement reading memory on Linux

---
+++ DevilDaggersCustomLeaderboards/Memory/Linux/LinuxHeapAccessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace DevilDaggersCustomLeaderboards.Memory.Linux
+{
+	public class LinuxHeapAccessor : IDisposable
+	{
+		private bool _disposedValue;
+
+		public LinuxHeapAccessor(string memoryFilePath)
+		{
+			Stream = new(memoryFilePath, FileMode.Open);
+		}
+
+		public FileStream Stream { get; }
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposedValue)
+			{
+				if (disposing)
+					Stream.Dispose();
+
+				_disposedValue = true;
+			}
+		}
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
--- DevilDaggersCustomLeaderboards/Utils/OperatingSystemUtils.cs
+++ DevilDaggersCustomLeaderboards/Utils/OperatingSystemUtils.cs
@@ -1,7 +1,10 @@
 ﻿using DevilDaggersCustomLeaderboards.Exceptions;
+using DevilDaggersCustomLeaderboards.Memory.Linux;
 using DevilDaggersCustomLeaderboards.Native;
 using System;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Runtime.InteropServices;
 using Os = DevilDaggersCustomLeaderboards.Clients.OperatingSystem;
 
@@ -9,6 +12,8 @@ namespace DevilDaggersCustomLeaderboards.Utils
 {
 	public static class OperatingSystemUtils
 	{
+		private static LinuxHeapAccessor? _linuxHeapAccessor;
+
 		static OperatingSystemUtils()
 		{
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
@@ -43,7 +48,16 @@ namespace DevilDaggersCustomLeaderboards.Utils
 					NativeMethods.ReadProcessMemory(process.Handle, new(address), bytes, (uint)size, out _);
 					break;
 				case Os.Linux:
-					// TODO
+					if (_linuxHeapAccessor != null)
+					{
+						_linuxHeapAccessor.Stream.Seek(address, SeekOrigin.Begin);
+						_linuxHeapAccessor.Stream.Read(bytes, 0, size);
+					}
+					else
+					{
+						Console.WriteLine("Linux: Cannot read memory block when heap is not accessible.");
+					}
+
 					break;
 				default:
 					throw new OperatingSystemNotSupportedException();
@@ -63,13 +77,65 @@ namespace DevilDaggersCustomLeaderboards.Utils
 			}
 			else if (OperatingSystem == Os.Linux)
 			{
-				// TODO
-				return null;
+				try
+				{
+					return BruteForceFindMarkerOnLinux(process);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Linux: Error while trying to find memory block:\n{ex.Message}");
+					return null;
+				}
 			}
 			else
 			{
 				throw new OperatingSystemNotSupportedException();
 			}
+		}
+
+		private static long BruteForceFindMarkerOnLinux(Process process)
+		{
+			string[] mapLines = File.ReadAllLines($"/proc/{process.Id}/maps");
+			string heapLine = Array.Find(mapLines, s => s.Contains("heap")) ?? throw new("No heap line in map file.");
+			string[] heapInfo = heapLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+			string[] addressInfo = heapInfo[0].Split('-');
+			if (!heapInfo[1].Contains('r'))
+				throw new("No read permissions for heap.");
+
+			_linuxHeapAccessor = new($"/proc/{process.Id}/mem");
+			long heapStart = long.Parse(addressInfo[0], NumberStyles.HexNumber);
+			long heapEnd = long.Parse(addressInfo[1], NumberStyles.HexNumber);
+			long total = heapEnd - heapStart;
+			_linuxHeapAccessor.Stream.Seek(heapStart, SeekOrigin.Begin);
+
+			const int chunkSize = 1024 * 1024 * 8;
+			const int markerSize = 12;
+			byte[] markerConst = new byte[markerSize] { 0x5F, 0x5F, 0x64, 0x64, 0x73, 0x74, 0x61, 0x74, 0x73, 0x5F, 0x5F, 0x00 };
+			byte[] chunk = new byte[chunkSize];
+
+			for (long bytesRead = 0; bytesRead < total; bytesRead += chunkSize)
+			{
+				_linuxHeapAccessor.Stream.Read(chunk, 0, chunk.Length);
+
+				int highestConsecutive = 0;
+				int consecutive = 0;
+				for (int i = 0; i < chunkSize; i++)
+				{
+					if (consecutive >= markerSize)
+						return heapStart + bytesRead + i - markerSize;
+
+					if (chunk[i] == markerConst[consecutive])
+						consecutive++;
+					else
+						consecutive = 0;
+
+					if (highestConsecutive < consecutive)
+						highestConsecutive = consecutive;
+				}
+			}
+
+			return 0;
 		}
 	}
 }
